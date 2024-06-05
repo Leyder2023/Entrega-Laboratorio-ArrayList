fb1e2eadaf2ca94ff4f3ab4daf501c63a692ebb7
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-05 09:38:33 -0500

    Add files via upload

---
+++ profesorEstudiante/src/main/java/com/mycompany/profesorestudiante/ProfesorEstudiante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.profesorestudiante;
+
+/**
+ *
+ * @author john
+ */
+public class ProfesorEstudiante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
